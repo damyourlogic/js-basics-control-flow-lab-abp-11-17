7e56119e895ef52dd7df43f9904b2a90e1430d15
Amy Jackson <aj81623@gmail.com>
2017-11-08 14:34:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code in this file!
+function scuberGreetingForFeet(number){
+  if (number <= 400){
+return ('This one is on me!');
+}
+  else if (number >= 2500){
+    return ('No can do.')
+  }
+  else {
+    return ('I will gladly take your thirty bucks.')
+}
+}
